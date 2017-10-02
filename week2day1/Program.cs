1c8c8aaf007ef888ce88c56ea1d52729d42a01e7
KaitieFox <kaitiefox@gmail.com>
2017-10-02 10:53:26 -0400

    Arrays

--- week2day1/Program.cs
+++ week2day1/Program.cs
@@ -10,6 +10,24 @@ namespace week2day1
     {
         static void Main(string[] args)
         {
+            //Declaring. Just saying it exists.
+            //int favoriteNumber;
+            //Initializing. It's the "equals"
+            // int favoriteNumber = "1"
+
+            //declare an array
+            //int[] favoriteNumbers;
+            //Jarryd pluralized. Because when we talk about arrays we are talking about many things of the same type.
+            //This plural is for us, the humans, to make sense of it.
+
+            //initialize the array
+            int[] favoriteNumbers = { 1, 2, 3, 4, 5, 6, };
+
+            string[] studentNames = { "Raz", "Monica", "David", "Brian", "Ali" };
+
+            Console.WriteLine();
+
+
         }
     }
 }
